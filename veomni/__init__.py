c87a6fcaaf6aad4a5378b29d2d5e0ef945f3fb62
zhihaofang1017 <zhihaofang1017@outlook.com>
2025-10-30 19:41:39 +0800

    Add RMSNorm and Rope ops

--- veomni/__init__.py
+++ veomni/__init__.py
@@ -15,7 +15,10 @@ from .utils.import_utils import (
     is_veomni_patch_available,
 )
 from .utils.logging import get_logger
-
+import torch
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as get_version
+from packaging.version import parse as parse_version
 
 logger = get_logger(__name__)
 
@@ -33,3 +36,32 @@ def _safe_apply_patches():
 _safe_apply_patches()
 
 __version__ = "v0.1.0"
+
+
+def is_torch_npu_available() -> bool:
+    """Check the availability of NPU"""
+    try:
+        import torch_npu  # noqa: F401
+        return torch.npu.is_available()
+    except ImportError:
+        return False
+    
+    
+is_npu_available = is_torch_npu_available()
+if is_npu_available:
+    from .models.transformers import npu_patch as npu_patch
+    package_name = "transformers"
+    required_version_spec = "4.50.4"
+    try:
+        installed_version = get_version(package_name)
+        installed = parse_version(installed_version)
+        required = parse_version(required_version_spec)
+        if installed < required:
+            raise ValueError(
+                f"{package_name} version >= {required_version_spec} is required on ASCEND NPU, current version is "
+                f"{installed}."
+            )
+    except PackageNotFoundError as e:
+        raise ImportError(
+            f"package {package_name} is not installed, please run pip install {package_name}=={required_version_spec}"
+        ) from e
