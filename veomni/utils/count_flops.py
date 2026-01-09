107efece15130b166f63d9cf6a9ebae9955c8cc4
ZhuYajun <1490663933@qq.com>
2026-01-09 18:08:31 +0800

    [config] feat:  add MFU calculation for qwen3_vl_moe (#385)

--- veomni/utils/count_flops.py
+++ veomni/utils/count_flops.py
@@ -71,6 +71,7 @@ class VeomniFlopsCounter:
             "qwen2_5_vl": self._estimate_qwen2_vl_flops,
             # qwen3_vl's vit uses full self attention while qwen2-vl/qwen2.5-vl uses window attention.
             "qwen3_vl": self._estimate_qwen3_vl_flops,
+            "qwen3_vl_moe": self._estimate_qwen3_vl_moe_flops,
             "deepseek_v3": self._estimate_deepseek_v3_flops,
             "qwen3_moe": self._estimate_qwen3_moe_flops,
             "llama": self._estimate_llama_flops,
@@ -353,6 +354,54 @@ class VeomniFlopsCounter:
         else:
             vit_flops = 0
 
+        # all_layer & all_token fwd & bwd flops
+        flops_all_token = dense_N_flops + attn_qkv_flops + vit_flops
+        flops_achieved = flops_all_token * (1.0 / delta_time) / 1e12
+        return flops_achieved
+
+    def _estimate_qwen3_vl_moe_flops(self, tokens_sum, batch_seqlens, delta_time, **kargs):
+        # qwen3_vl uses text_config and vision_config to distinguish configs of different parts.
+        hidden_size = self.config.text_config.hidden_size
+        vocab_size = self.config.text_config.vocab_size
+        moe_intermediate_size = self.config.text_config.moe_intermediate_size
+        num_hidden_layers = self.config.text_config.num_hidden_layers
+        num_key_value_heads = self.config.text_config.num_key_value_heads
+        num_attention_heads = self.config.text_config.num_attention_heads
+        moe_intermediate_size = self.config.text_config.moe_intermediate_size
+        moe_num_expert = self.config.text_config.num_experts
+        moe_topk = self.config.text_config.num_experts_per_tok
+        head_dim = getattr(
+            self.config.text_config,
+            "head_dim",
+            self.config.text_config.hidden_size // self.config.text_config.num_attention_heads,
+        )
+        q_size = num_attention_heads * head_dim
+        k_size = num_key_value_heads * head_dim
+        v_size = num_key_value_heads * head_dim
+        # non-attn per layer parm
+        moe_gata_N = hidden_size * moe_num_expert
+        # moe has gate_proj, up_proj and down_proj using SwiGLU in ExpertMlp layer & shared experts
+        moe_expertmlp_N = hidden_size * moe_intermediate_size * (moe_topk) * 3
+        attn_linear_N = hidden_size * (q_size + k_size + v_size + num_attention_heads * head_dim)
+        emd_and_lm_head_N = vocab_size * hidden_size * 2
+        # non-attn all_layer parm
+        moe_N = (moe_gata_N + moe_expertmlp_N + attn_linear_N) * (num_hidden_layers) + emd_and_lm_head_N
+        # non-attn all_layer & all_token fwd & bwd flops
+        dense_N_flops = 6 * moe_N * tokens_sum
+        # attn all_layer & all_token fwd & bwd flops
+        seqlen_square_sum = 0
+        for seqlen in batch_seqlens:
+            seqlen_square_sum += seqlen * seqlen
+        attn_qkv_flops = 12 * seqlen_square_sum * head_dim * num_attention_heads * num_hidden_layers
+        # all_layer & all_token fwd & bwk flops
+        flops_all_token = dense_N_flops + attn_qkv_flops
+        flops_achieved = flops_all_token * (1.0 / delta_time) / 1e12
+        # vit flops
+        image_seqlens = kargs.get("image_seqlens", None)
+        if image_seqlens is not None:
+            vit_flops = self._estimate_qwen3_vit_flop(image_seqlens, self.config.vision_config)
+        else:
+            vit_flops = 0
         # all_layer & all_token fwd & bwd flops
         flops_all_token = dense_N_flops + attn_qkv_flops + vit_flops
         flops_achieved = flops_all_token * (1.0 / delta_time) / 1e12
