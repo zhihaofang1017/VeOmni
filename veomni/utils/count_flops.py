aa88bf4acce221891edbd782b2f613ed8d9bb70f
HSYZhang <127824860+HSYZhang@users.noreply.github.com>
2026-01-29 02:27:11 +0800

    [perf] fix: The H200 compute power is being recognized as H20 (#428)
    Co-authored-by: zhanglong.443 <zhanglong.443@jd.com>

--- veomni/utils/count_flops.py
+++ veomni/utils/count_flops.py
@@ -35,7 +35,7 @@ def get_device_flops(unit="T"):
 
     device_name = get_device_name()
     flops = float("inf")  # INF flops for unkown gpu type
-    if "H100" in device_name or "H800" in device_name:
+    if "H100" in device_name or "H800" in device_name or "H200" in device_name:
         flops = 989e12
     elif "A100" in device_name or "A800" in device_name:
         flops = 312e12
